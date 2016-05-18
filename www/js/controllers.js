a7a05871b8f63867fc07846debacd7742b9a9282
bradguillen15 <bradguillen15@gmail.com>
2016-05-17 21:57:14 -0600

    angular

---
+++ www/js/controllers.js
@@ -0,0 +1,8 @@
+(function(){
+    var app = angular.module("SmarketController", []);
+    
+    var IndexController = function(){
+    };
+    
+    app.controller("IndexController", [IndexController]);
+}());
